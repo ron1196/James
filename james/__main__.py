28cfc39454ba48fe03b219d4e282032f9483b2d4
Ron Mizrachi <ron1196@gmail.com>
2018-09-23 02:58:54 +0300

    Update

--- james/__main__.py
+++ james/__main__.py
@@ -7,14 +7,19 @@ import difflib
 import sys
 import pickle
 import math
+import re
+from urllib.request import Request, urlopen
+from datetime import datetime
 
-TOKEN = 'NDM3MzIxOTQ3NDgzODY1MDk4.Db0YAg.PLZIhuXcnzH5XyA2xsXuU2oSObY'
-James = commands.Bot(command_prefix='!', owner_id=195097871626928128, case_insensitive=True)
-James.remove_command('help')
-
-gyms = {}
+CITY = ['jer', 'ash', 'mad']
 
 def load_data():
+    # Load configuration
+    global config
+    with open('config.json', 'r') as fd:
+        config = json.load(fd)
+    
+    # Load Guild Dict
     global guild_dict
     try:
         with open(os.path.join('data', 'guild_dict'), 'rb') as fd:
@@ -24,18 +29,26 @@ def load_data():
             guild_dict = {}
             pickle.dump(guild_dict, fd, (- 1))
             
+    # Load Gyms
     global gyms
-    with open(os.path.join('data', 'gyms.json'), 'r') as fd:
-        gyms_data = json.load(fd)["Document"]["Folder"]
-    for gym_folder in gyms_data:
-        for gym in gym_folder["Placemark"]:
-            gym_name = str(gym["name"]) if ("__cdata" not in gym["name"]) else str(gym["name"]["__cdata"])
-            gym_coord = gym["Point"]["coordinates"].strip().split(',')[:2]
-            gym_coord.reverse()
-            gym_ex_confirmed = True if gym_folder["name"] == "Confirmed EX Gyms" else False
+    gyms = {}
+    for city in CITY:
+        gyms[city] = {}
+        with open(os.path.join('data/gyms', f'{city}.json'), 'r') as fd:
+            gyms_data = json.load(fd)["Document"]["Folder"]
+        for gym_folder in gyms_data:
+            if( gym_folder['name'] == "Gyms" or gym_folder['name'] == "Confirmed EX Gyms" ):
+                for gym in gym_folder["Placemark"]:
+                    gym_name = str(gym["name"]) if ("__cdata" not in gym["name"]) else str(gym["name"]["__cdata"])
+                    gym_coord = gym["Point"]["coordinates"].strip().split(',')[:2]
+                    gym_coord.reverse()
+                    gym_ex_confirmed = True if gym_folder["name"] == "Confirmed EX Gyms" else False
+                    
+                    gyms[city][gym_name.lower()] = {"Name": gym_name, "Coordinates": gym_coord, "Ex Confirmed": gym_ex_confirmed}
             
-            gyms[gym_name.lower()] = {"Name": gym_name, "Coordinates": gym_coord, "Ex Confirmed": gym_ex_confirmed}
 load_data()
+James = commands.Bot(command_prefix=config['default_prefix'], owner_id=config['master'], case_insensitive=True)
+James.remove_command('help')
 
 async def save():
     with open(os.path.join('data', 'guild_dict_tmp'), 'wb') as fd:
@@ -84,8 +97,10 @@ async def on_guild_remove(guild):
 @James.event
 async def on_guild_channel_create(channel):
     if channel.guild != None:
-        if channel.category.name.lower() == "raids":
+        name = channel.name
+        if name.startswith("ex") or (name[0].isdigit() and int(name[0]) >= 1 and int(name[0]) <= 5) or name.startswith("level"):
             await asyncio.sleep(7)
+            
             first_message = (await channel.history(reverse=True).flatten())[0]
             details_start_index = first_message.content.index("Details: ") + len("Details: ")
             details_end_index = first_message.content.index(".", details_start_index)
@@ -98,19 +113,25 @@ async def on_guild_channel_create(channel):
                 if gym["Ex Confirmed"]:
                     role = discord.utils.get(channel.guild.roles, name="mewtwo")
                     await channel.send('{}Confirmed EX Gym in {}'.format((role.mention + " - ") if role else "", raid_location_details))
+            
 
 """
 Helper functions
 """  
 
 async def find_gym(entered_gym, author, channel):
+    guild = channel.guild
+    if( guild_dict[guild.id].get("region", None) is None ):
+        await channel.send("Use **!region [region]**")
+        return
+        
     entered_gym = entered_gym.lower()
-    gym = gyms.get(entered_gym, None)
+    gym = gyms[guild_dict[guild.id]["region"]].get(entered_gym, None)
     if not gym:
-        gym_autocorrect = autocorrect(entered_gym, gyms.keys(), author, channel)
+        gym_autocorrect = autocorrect(entered_gym, gyms[guild_dict[guild.id]["region"]].keys(), author, channel)
         if gym_autocorrect:
             if await ask('The Gym name is {} ?'.format(gym_autocorrect.title()), author, channel):
-                gym = gyms[gym_autocorrect]
+                gym = gyms[guild_dict[guild.id]["region"]][gym_autocorrect]
     if not gym:
         pass#await channel.send("Use **!gym [gym_name]** to send the gym location.")
     return gym
@@ -173,7 +194,7 @@ async def gym(ctx):
     if gym:
         maps_link = 'https://www.google.com/maps/search/?api=1&query={}'.format('+'.join(gym['Coordinates']))
         await channel.send(f'{maps_link}')
-        if gym["Ex Confirmed"]:
+        if ("-" in channel.name) and gym["Ex Confirmed"]:
             role = discord.utils.get(channel.guild.roles, name="mewtwo")
             await channel.send('{}Confirmed EX Gym in {}'.format((role.mention + " - ") if role else "", entered_gym))
 
@@ -205,8 +226,50 @@ async def region(ctx):
         await channel.send('Give me a region!')
         return
     region = ' '.join(args)
-    guild_dict[guild.id]['region'] = region
-    await message.add_reaction('☑')
+    if (region in CITY):
+        guild_dict[guild.id]['region'] = region
+        await message.add_reaction('✅')
+    else:
+        await message.add_reaction('❎')
+    
+@James.command()
+async def events(ctx):
+    message = ctx.message
+    channel = message.channel
+    guild = channel.guild
+    
+    events_url = "https://pokemon.gameinfo.io/en/js/list-events.js?v4.34.2"
+    request = Request(events_url, headers={'User-Agent': 'Mozilla/5.0'})
+    response = urlopen(request)
+    html = response.read().decode("utf-8") 
+    
+    start = html.index("_event_data=") + len("_event_data=")
+    end = html.index("if(!hasStorage)")
+    html_array = html[start:end-1]
+    events = json.loads(html_array)
+
+    event_upcoming = [] 
+    for event in events:
+        if event.get('start', None):
+            start_date = datetime.fromtimestamp(event['start'])
+            if start_date >= datetime.now():
+                event_upcoming.append( (event['name'], start_date) )
+            continue
+        
+        if event.get('day', None) and event['day'] is not None:
+            start_date = datetime.strptime(event['day'][0], "%Y-%m-%d")
+            if start_date >= datetime.now():
+                event_upcoming.append( (event['name'], start_date) )
+            continue
+        
+    event_upcoming.sort(key=lambda x: x[1])
+    text = ""
+    for event in event_upcoming:
+        text += "{} - {}\n".format(event[0], event[1].strftime('%d/%m/%Y'))
+    
+    embed = discord.Embed(title="Events", description=text, colour=guild.me.colour)
+    await channel.send(embed=embed)
+    
     
 """
 Admin Commands
@@ -246,7 +309,7 @@ async def exit(ctx):
     
     
 try:
-    event_loop.run_until_complete(James.start(TOKEN))
+    event_loop.run_until_complete(James.start(config['bot_token']))
 except discord.LoginFailure:
     # Invalid token
     event_loop.run_until_complete(James.logout())
