0e353092dd9b2b83eb35bc728f19cf7e12d2a57f
Ron Mizrachi <ron1196@gmail.com>
2018-09-23 02:57:57 +0300

    Update

--- launcher.py
+++ launcher.py
@@ -2,24 +2,33 @@
 
 import sys
 import os
+import time
 import subprocess
 import argparse
 
 #Launcher for James
 
 def parse_cli_args():
-    parser = argparse.ArgumentParser(description="James Launcher - Pokemon Go Bot for Discord")
-    parser.add_argument("--start","-s",help="Starts James",action="store_true")
-    parser.add_argument("--auto-restart","-r",help="Auto-Restarts James in case of a crash.",action="store_true")
-    parser.add_argument("--debug","-d",help="Prevents output being sent to Discord DM, as restarting could occur often.",action="store_true")
+    parser = argparse.ArgumentParser(
+        description="James Launcher - Pokemon Go Bot for Discord")
+    parser.add_argument(
+        "--auto-restart", "-r",
+        help="Auto-Restarts james in case of a crash.", action="store_true")
+    parser.add_argument(
+        "--debug", "-d",
+        help=("Prevents output being sent to Discord DM, "
+              "as restarting could occur often."),
+        action="store_true")
     return parser.parse_args()
 
-def run_James(autorestart):
+def run_james(autorestart):
     interpreter = sys.executable
     if interpreter is None:
         raise RuntimeError("Python could not be found")
 
-    cmd = [interpreter, "james", "launcher"]
+    cmd = [interpreter, "-m", "james", "launcher"]
+
+    retries = 0
 
     while True:
         if args.debug:
@@ -34,6 +43,7 @@ def run_James(autorestart):
                 break
             elif code == 26:
                 #standard restart
+                retries = 0
                 print("")
                 print("Restarting James")
                 print("")
@@ -41,9 +51,17 @@ def run_James(autorestart):
             else:
                 if not autorestart:
                     break
+                retries += 1
+                wait_time = min([retries^2, 60])
                 print("")
-                print("Restarting James from crash")
+                print("James experienced a crash.")
                 print("")
+                for i in range(wait_time, 0, -1):
+                    sys.stdout.write("\r")
+                    sys.stdout.write(
+                        "Restarting James from crash in {:0d}".format(i))
+                    sys.stdout.flush()
+                    time.sleep(1)
 
     print("James has closed. Exit code: {exit_code}".format(exit_code=code))
 
@@ -53,7 +71,5 @@ if __name__ == '__main__':
     abspath = os.path.abspath(__file__)
     dirname = os.path.dirname(abspath)
     os.chdir(dirname)
-
-    if args.start:
-        print("Launching James...")
-        run_James(autorestart=args.auto_restart)
+    print("Launching James...")
+    run_james(autorestart=args.auto_restart)
